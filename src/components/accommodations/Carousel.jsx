8cc105ee76e00eefffb1d2281c486857d2bc7226
Tracy Joyner <tracyjoyner444@gmail.com>
2024-10-29 16:20:21 -0500

    progressing on accommocation page

--- src/components/accommodations/Carousel.jsx
+++ src/components/accommodations/Carousel.jsx
@@ -1,42 +1,42 @@
-// import { useState } from "react";
-// import arrow from "../../assets/Carousel-Arrow.svg";
+import { useState } from "react";
+import arrow from "../../assets/Carousel-Arrow.svg";
 
-// function Carousel({ pictures }) {
-//   const [current, setCurrent] = useState(0);
-//   const nextPicture = () =>
-//     setCurrent(current === pictures.length - 1 ? 0 : current + 1);
-//   const previousPicture = () =>
-//     setCurrent(current === 0 ? pictures.length - 1 : current - 1);
+function Carousel({ pictures }) {
+  const [current, setCurrent] = useState(0);
+  const nextPicture = () =>
+    setCurrent(current === pictures.length - 1 ? 0 : current + 1);
+  const previousPicture = () =>
+    setCurrent(current === 0 ? pictures.length - 1 : current - 1);
 
-//   if (!Array.isArray(pictures) || pictures.length <= 0) {
-//     return null;
-//   }
+  if (!Array.isArray(pictures) || pictures.length <= 0) {
+    return null;
+  }
 
-//   return (
-//     <div className="carousel-container">
-//       {pictures.length === 1 ? (
-//         <img src={pictures[0]} alt="" />
-//       ) : (
-//         <>
-//           {pictures.map((picture, index) => (
-//             <img
-//               key={index}
-//               className={"picture-" + (index === current ? "active" : "")}
-//               src={picture}
-//               alt=""
-//               caption={`${index + 1}/${pictures.length}`}
-//             />
-//           ))}
-//           <div className="arrow-previous" onClick={previousPicture}>
-//             <img src={arrow} className="previous-arrow" alt="previouse arrow" />
-//           </div>
-//           <div className="arrow-next" onClick={nextPicture}>
-//             <img src={arrow} className="next-arrow" alt="next arrow" />
-//           </div>
-//         </>
-//       )}
-//     </div>
-//   );
-// }
+  return (
+    <div className="carousel-container">
+      {pictures.length === 1 ? (
+        <img src={pictures[0]} alt="" />
+      ) : (
+        <>
+          {pictures.map((picture, index) => (
+            <img
+              key={index}
+              className={"picture" + (index === current ? " active" : "")}
+              src={picture}
+              alt=""
+              caption={`${index + 1}/${pictures.length}`}
+            />
+          ))}
+          <div className="arrow-previous" onClick={previousPicture}>
+            <img src={arrow} className="previous-arrow" alt="previouse arrow" />
+          </div>
+          <div className="arrow-next" onClick={nextPicture}>
+            <img src={arrow} className="next-arrow" alt="next arrow" />
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
 
-// export default Carousel;
+export default Carousel;
